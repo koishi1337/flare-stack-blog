34309557aa3da1f6c85338089ff077d366935eb7
koishi1337 <103472975+koishi1337@users.noreply.github.com>
2026-07-15 20:22:29 +0800

    Update root-handler.ts

--- src/lib/worker/root-handler.ts
+++ src/lib/worker/root-handler.ts
@@ -2,6 +2,7 @@ import { OAuthProvider } from "@cloudflare/workers-oauth-provider";
 import { McpApiHandler } from "@/features/mcp/api/mcp-api-handler";
 import { createWorkersOAuthProviderOptions } from "@/features/oauth-provider/oauth-provider.config";
 import { appWorkerHandler } from "./app-handler";
+ import { handleUsersRequest } from '@/features/users/api/users-api.handler';
 
 let oauthProvider: OAuthProvider<Env> | null = null;
 
